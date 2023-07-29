b4fd1e82109996b7bd07337211cf7f14966891ee
hoanbaby <140515699+hoanbaby@users.noreply.github.com>
2023-07-29 23:01:33 +0700

    Update firebase-config.js

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyAe2IUOUKB6UnbAa45JZkP3psHe0zKTpvQ",
-   authDomain: "d4tmp3-reactjs-7195d.firebaseapp.com",
+   apiKey: "AIzaSyAe2IUOUKB6UnbAa45JZkP3psHe0zKTpv",
+   authDomain: "d4tmp3-reactjs-7195d.firebaseapp.co",
    projectId: "d4tmp3-reactjs-7195d",
-   storageBucket: "d4tmp3-reactjs-7195d.appspot.com",
-   messagingSenderId: "278324459087",
-   appId: "1:278324459087:web:cd5aa4d5f1fccd2e149c3a",
+   storageBucket: "d4tmp3-reactjs-7195d.appspot.co",
+   messagingSenderId: "27832445908",
+   appId: "1:278324459087:web:cd5aa4d5f1fccd2e149c3",
 }
 
 // Initialize Firebase
